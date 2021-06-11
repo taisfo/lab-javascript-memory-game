e51f86fa89e820ec9470fe9910d4bfff365c2c4a
Taís Orsini <taisforsini@gmail.com>
2021-06-11 07:37:55 -0300

    done class

--- js/memory.js
+++ js/memory.js
@@ -1,15 +1,25 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    this.cards.sort(() => Math.random() - 0.5);
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    card1 = this.pickedCards[0];
+    card2 = this.pickedCards[1];
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   checkIfFinished() {
